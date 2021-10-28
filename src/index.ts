e9185e1cd27bf53193a7509167f080fc0d2bd64a
Leo Vigna <leo.vigna@gmail.com>
2021-10-28 12:11:55 -0400

    initial starter-redux from starter-typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { NAME } from './utils/environment';
-import hello from './hello';
-
 function main() {
-    console.debug(hello(NAME));
 }
 
 if (typeof require !== 'undefined' && require.main === module) {
--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,7 +1,6 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            NAME?: string;
             NODE_ENV: 'development' | 'production' | 'test';
         }
     }
@@ -9,4 +8,4 @@ declare global {
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export {};
+export { };
