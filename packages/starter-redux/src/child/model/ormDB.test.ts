924f2c3eedc254bda480307776a8a83b75de3e48
Leo Vigna <leo.vigna@gmail.com>
2022-07-05 12:35:26 +0400

    update test

--- packages/starter-redux/src/child/model/ormDB.test.ts
+++ packages/starter-redux/src/child/model/ormDB.test.ts
@@ -1,4 +1,5 @@
 import { assert } from 'chai';
+import { Connector } from 'indexeddb-orm';
 import { Interface, validate } from './interface.js';
 import { name } from '../common.js';
 import getDB from '../../db.js';
@@ -8,13 +9,15 @@ describe(`${name}/model/ormDB.ts`, () => {
 
     // eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-commonjs
     const FDBFactory = require('fake-indexeddb/lib/FDBFactory');
+    let db: Connector;
 
     beforeEach(async () => {
         indexedDB = new FDBFactory();
+        db = await getDB();
     });
 
     it('insert', async () => {
-        const models = await (await getDB()).connect();
+        const models = await db.connect();
         const record = await models[name].create(item);
         assert.deepEqual(record, item);
     });
